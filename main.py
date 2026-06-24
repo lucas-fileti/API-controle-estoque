7304837e05f4a2f3851ce3f39ecadd74d0d917b6
lucas fileti <lucasfiletii@gmail.com>
2026-06-23 21:24:52 -0300

    add categorias

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from routes.categorias import router as categorias_router
 
+app = FastAPI(
+    title="API Stock Control",
+    description="API para controle de estoque",
+    version="1.0.0"
+)
 
-app = FastAPI()
-
+app.include_router(
+    categorias_router,
+    prefix="/api",
+    tags=["Categorias"]
+)
 
 @app.get("/")
 def home():
